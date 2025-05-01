e8962fc1b94e89f690d25c472a243fb5691990bd
Tadeusz Kielak <tadeusz@kielak.com>
2025-05-01 09:22:07 +0000

    dodanie do allowedDevOrigins umowydev.kielak.com

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['umowydev.kielak.com'],
 };
 
 export default nextConfig;
