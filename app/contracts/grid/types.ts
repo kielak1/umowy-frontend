a023d7abec90414dfc2b5b25c938a048e82fc551
Tadeusz Kielak <tadeusz@kielak.com>
2025-05-05 17:48:10 +0000

    rozszerzenie typów

--- app/contracts/grid/types.ts
+++ app/contracts/grid/types.ts
@@ -10,15 +10,58 @@ export interface OrganizationalUnit {
   id: number;
   name: string;
 }
+
+// Słowniki
+export interface SlownikKategoriaUmowy {
+  id: number;
+  nazwa: string;
+}
+
+export interface SlownikObszarFunkcjonalny {
+  id: number;
+  nazwa: string;
+}
+
+export interface SlownikWlasciciel {
+  id: number;
+  nazwa: string;
+}
+
+export interface SlownikStatusUmowy {
+  id: number;
+  nazwa: string;
+}
+
+export interface SlownikKlasyfikacjaUmowy {
+  id: number;
+  nazwa: string;
+}
+
 export interface ZmianaUmowy {
   id: number;
-  rodzaj: string;
+  rodzaj: "umowa" | "aneks" | "porozumienie" | "inne";
   data_zawarcia: string;
-  kwota_netto: string;
-  waluta: string;
-  przedmiot: string;  
-  status: string;
+  data_obowiazywania_od: string;
+  data_obowiazywania_do: string | null;
+  data_podpisania: string | null;
+  data_wypowiedzenia: string | null;
+  trzeba_wypowiedziec: boolean;
+  finansowanie_do: string | null;
+
+  przedmiot: string | null;
+  producenci: string | null;
+  numer_umowy_dostawcy: string | null;
+  kwota_netto: string | null;
+  waluta: "PLN" | "EUR" | "USD";
+  opis: string;
+
+  kategoria: SlownikKategoriaUmowy | null;
+  obszary_funkcjonalne: SlownikObszarFunkcjonalny[];
+  wlasciciel: SlownikWlasciciel | null;
+  status: SlownikStatusUmowy | null;
+  klasyfikacja: SlownikKlasyfikacjaUmowy | null;
 }
+
 export interface Umowa {
   id: number;
   numer: string;
@@ -27,10 +70,15 @@ export interface Umowa {
   czy_spelnia_dora: boolean;
   czy_wymaga_kontynuacji: boolean;
   wymagana_data_kontynuacji: string | null;
+
   kontrahent: Kontrahent | null;
   opiekun: User | null;
   jednostka_organizacyjna: OrganizationalUnit | null;
+
   najnowsza_zmiana: ZmianaUmowy | null;
+  zamowienia?: Zamowienie[];
+  zmiany?: ZmianaUmowy[];
+
   _expanded?: boolean | "inline";
 }
 
@@ -40,6 +88,9 @@ export interface Zamowienie {
   data_zlozenia: string;
   data_realizacji: string | null;
   kwota_netto: string;
-  waluta: string;
+  waluta: "PLN" | "EUR" | "USD";
   opis: string;
+
+  przedmiot: string | null;
+  producenci: string | null;
 }
